d38f448efd6bc3262f2a9adde808f8f82bc074d7
nisarlala0987-boop <nisarlala0987@gmail.com>
2026-03-01 00:26:48 +0500

    comm

---
+++ 12/2025/11/11/12/11/12/2025/index.js
@@ -0,0 +1,142 @@
+// alert("or loro")
+
+// let num=[12,13,14,15,16]
+
+// let sum=0;
+// for (let value of num) {
+//     sum=sum+value;
+// }
+// let avg=sum / num.length;
+// console.log(avg)
+
+
+// let item=[200,300,400,100,500]
+// let i=0;
+// for (let val of item) {
+
+//     let offer =val/10;
+// item[i]=item[i]-offer;
+//     console.log( item[i])  
+//     i++;
+    
+// }
+// for (let i = 0; i < item.length; i++) {
+//     let offer=item[i]/10;
+// item[i]=item[i]-offer;
+
+// console.log(item[i])    
+// }
+// var food=["amreed","keela","anaar","seeeb"]
+// console.log(food);
+// console.log(food.toString());
+
+// var hero=["zohan","wakeel","sabir"];
+// var heromerijan=['nisar',"wakeel","sabir"]
+// var lulimaster=['rao',"moix","ahmed"]
+
+// let sarehero=hero .concat(heromerijan,lulimaster)
+// console.log(sarehero);
+
+// var heromerijan=['nisar',"wakeel","sabir"]
+// heromerijan.shift()
+// console.log(heromerijan)
+
+// var heromerijan=['nisar',"wakeel","sabir"]
+// heromerijan.unshift("moiz")
+// console.log("addedvalue=",heromerijan)
+
+
+// var heromerijan=['nisar',"wakeel","sabir"]
+// heromerijan.splice(3,0,3,3,3,3)
+//  console.log(heromerijan)
+
+// var heromerijan=['nisar',"wakeel","sabir","moiz"]
+// heromerijan.splice(3,1)
+//  console.log(heromerijan)
+
+
+// var heromerijan=['nisar',"wakeel","sabir","moiz"]
+
+//  heromerijan.slice(2)
+//  console.log(heromerijan)
+
+
+// let companies=["nisar","sabir","wakeel","zohan"]
+// console.log(companies)
+
+
+// let companies=["nisar","sabir","wakeel","zohan"]
+// companies.shift()
+// console.log(companies)
+
+// let companies=["nisar","sabir","wakeel","zohan"]
+// companies.splice(0,1,'moiz')
+// console.log(companies)
+// 
+
+// let companies=["nisar","sabir","wakeel","zohan"]
+// companies.push('moiz')
+// console.log(companies)
+
+// let companies=["nisar","sabir","wakeel","zohan"]
+// companies.pop()
+// console.log(companies)
+
+
+// let math=Math.round(Math.random()*5);
+// console.log(math);
+
+// let math=Math.ceil(.111);
+// console.log(math);
+// function myfunction(params) {
+//     var num=23;
+//    var user= Number(prompt("search kar kuch loro"))
+//    while (user!==num) {
+
+//    var user=Number(prompt("wapis search kar loro"));
+    
+// } 
+//     alert("its ok lulli");
+// }
+// myfunction();
+
+// let val=23;
+// var user=Number(prompt("num guess kar chutye"));
+// while (user!==val) {
+
+// var user=Number(prompt("chutye galat hai "));
+    
+// }
+// alert("ye bat")
+
+// function myfunction(x,y) {
+//    console.log(x+y) ;
+// }
+
+// sum=1+3;
+// console.log(sum)
+
+// function sum(x,y) {
+//   s=x+y;
+//  return s; 
+
+
+// }
+// let val=sum(3,4)
+// console.log(val)
+
+function myfunction() {
+  var val=[1,2,3,4,5,6,7,8,9]
+
+for (let i = val.length -1; i >= 0; i--) {
+console.log(val[i])  
+}
+}
+
+
+myfunction();
+
+
+// var name="nisar";
+// var lastname="khan";
+// console.error(name+lastname)
--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,193 +1,95 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
+  import { getAuth, 
+    createUserWithEmailAndPassword ,
+    onAuthStateChanged,
+    signInWithEmailAndPassword
+  } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
-    //  ALERT 
-// [1]
-// 1.
-// alert("hello")
-// 2.
-// alert("nisar khan")
-// 3.
-// var name='nisar';
-// alert(name)
-// 4.
-// let first="nisar"
-// let second="hello"
 
-// let combined=first+second;
-// alert(combined)
 
-    // Variable for string
-    // 2
-// 1.
-// let username="nisar"
-// console.log(username)
-// 2.
-// let message="welcome"
-// alert(message)
-// 3.
-// let user="nisar"+ " "+"khan"
-// console.log(user)
-// 4.
-// let favoritequote="i love to perform, Namaz"
-// console.log(favoritequote)
-// 5.
-// let city="welcome to our,(karachi)"
-// alert(city)
+
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyD6H0JXLi8aV8rApfmkDeZBgFQk2vGHzxM",
+    authDomain: "login-form-c178f.firebaseapp.com",
+    projectId: "login-form-c178f",
+    storageBucket: "login-form-c178f.firebasestorage.app",
+    messagingSenderId: "954559059965",
+    appId: "1:954559059965:web:80fa0f124c714d9d1bab94",
+    measurementId: "G-GRPE86DBXP"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  console.log(app)
+  const auth = getAuth(app);
+  console.log(auth)
+
+
+
+    
+    var no =document.getElementById("place")
+    var nom =document.getElementById("pss")
+    var nisra=document.getElementById("pass")
+    nisra.addEventListener("click",signup)
+
+
+      onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      var para=document.getElementById("output")
+      para.innerText=user.email
+
+    } else {
+      
+    }
+  });
+  
+function signup() {
+    
+createUserWithEmailAndPassword(auth, no.value,nom.value)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user)
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage)
+  });
+
+
+}
+
+
+var pasw=document.getElementById("pasw")
+var lala=document.getElementById("lala")
+var nisar=document.getElementById("login")
+nisar.addEventListener("click",login)
+
+function login() {
+signInWithEmailAndPassword(auth, lala.value, pasw.value)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log("hugya")
+    location.href="https://facebook.com"
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(" nhi hugya")
+
+  });
+
  
-
-// Variable For Numbers
-// 3
-// 1.
-// let age=18;
-// console.log(age);
-// 2.
-// let number1=18;
-// let number2=19;
-// let sum=number1 +number2;
-// console.log("answer=",sum)
-// 3.
-// let subtract1="18";
-// let subtract2="19";
-// let substract=subtract2-subtract1;
-// console.log("answer=",substract)
-// 4.
-// let sum1=5;
-// let sum2=10;
-// let sum=sum1*sum2;
-// console.log(sum)
-// 5.
-// let age=18;
-// alert("i am "+age+ " years old");
-
-// Math Expression
-    // 4
-// 1.
-// let x=10;
-// let y=10;
-// let sum=x+y;
-// console.log("result=",sum);
-// 2.
-// let x=10;
-// let y=20;
-// let substract=y-x;
-// alert(substract);
-// 3.
-// let x=5;
-// let y=10;
-// let multiply=x*y;
-// console.log("result=",multiply);
-// 4.
-// let x=10
-// let y =2
-// let divide=x/y;
-// console.log(divide);
-// 5.
-// let x=10
-// let y =3
-// let divide=x%y;
-// console.log(divide);
-
-
-    // Prompt
-   // 5
-// 1.
-// let user=prompt('what is your name?')
-// alert("hello"+" "+user)
-// 2.
-// let age =prompt('what is your age?' )
-// console.log(age)
-// 3.
-// let user=prompt("enter the number!")
-// let multiply=Number(user)*2
-// alert(multiply)
-// 4.
-// let favoritecolor=prompt("guess the colour");
-// alert("wow i also like"+" " +favoritecolor+ "  "+ "color");
-// 5.
-// let city=prompt("your city name");
-// alert("you are from"+"["+city+"]")
-
-// If Statement and Comparison Operators
-       // 6
-// 1.
-// let age=30
-// let user=prompt("enter your age")
-// if (user<=age) {
-//     console.log("you are still young")
-// }
-// else{
-//     console.log("you are old uncle" )
-// }
-// 2.
-// let num=prompt("enter the age");
-
-// if (num%2===0) {
-//     alert("even")
-// }
-// else{
-//     alert("odd")
-// }
-// 3.
-// let a=Number(prompt("enter first num"));
-// let b=Number(prompt("enter second num "));
-
-// if (a>b) {
-//     alert(a+"its greator")
-    
-// }
-// else if (b>a) {
-//     alert(b+"its greator")
-    
-// }
-// else{
-//     alert("its equal")
-// }
-
-
-// 4.
-// let pass="0316"
-// let user=prompt("enter the password");
-// if (user==pass) {
-
-// alert("Access granted")
-    
-// }
-// else{
-//     alert("incorrect password, try again!")
-// }
-
-// 5.
-    //   ==
-// let a = 5;
-
-// if (a == "5") {
-//     alert("values are equal");
-// }
-
-    //   ===
-// let b = 5;
-
-// if (b === "5") {
-//     alert("=== : values  equal");
-// } else {
-//     alert(" not equal ");
-// }
-
-        //   !=
-// let c = 5;
-
-// if (c != "6") {
-//     alert(" values are not equal");
-// }
-
-        // !==
-// let d = 5;
-// if (d !== "5") {
-//     alert(" value or type is different");
-// }
+}
 
   
 
 
 
 
- 
+
